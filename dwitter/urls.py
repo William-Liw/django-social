fd1614ae70929195a28809ae2e32be067554fb87
William Liw <william@thelessonspace.com>
2022-05-23 11:55:46 +0400

    corrects a bug for the login redirect. there is a discrepancy with account/accounts but is working now

--- dwitter/urls.py
+++ dwitter/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
-from .views import dashboard, profile_list, profile
+from django.urls import path, include
+from .views import dashboard, profile_list, profile, upload_file
 
 app_name = "dwitter"
 
 urlpatterns = [
-    path("", dashboard, name="dashboard",),
+    path("", dashboard, name="dashboard"),
     path("profile_list/", profile_list, name="profile_list"),
     path("profile/<int:pk>", profile, name="profile"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("upload/",upload_file, name="upload_file"),
 ]
