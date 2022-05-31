5ca295ca6416ab97e087e1bf81ed993344b41f45
William Liw <william@thelessonspace.com>
2022-05-31 09:42:42 +0400

    add media root and url in settings file

--- dwitter/urls.py
+++ dwitter/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from .views import dashboard, profile_list, profile, upload_file, QuoteView
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "dwitter"
 
@@ -10,4 +12,4 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     # path("upload/", upload_file, name="upload_file"),
     path("upload/", QuoteView.as_view(), name="upload_file"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social/settings.py
+++ social/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dwitter:dashboard'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'dwitter/quotes')
+MEDIA_URL = '/quotes/'
