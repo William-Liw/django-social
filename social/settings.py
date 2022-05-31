038410e09f9fba6f154d589af99c6dd674fdbb24
William Liw <william@thelessonspace.com>
2022-05-31 10:53:20 +0400

    uploads file to s3 and added .env file

--- social/settings.py
+++ social/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,5 +134,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dwitter:dashboard'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'dwitter/quotes')
-MEDIA_URL = '/quotes/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'dwitter/quotes')
+# MEDIA_URL = '/quotes/'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_REGION_NAME = 'us-east-1'
+# AWS_S3_ENDPOINT_URL = ''
+AWS_QUERYSTRING_AUTH = False
